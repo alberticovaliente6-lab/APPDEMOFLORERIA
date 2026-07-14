270c777ab4828856aa8bb13da09769bfca04d37c
alberticovaliente6-lab <alberticovaliente6@gmail.com>
2026-07-13 23:19:19 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.POS_CONFIG = window.POS_CONFIG || {
-  apiBaseUrl: "https://script.google.com/macros/s/AKfycbwrGKTZTTrJCRE5ElDClHGawTU9jPdZNxpYI0N1G2q5lTbwg4_XEqvae47kMpnF8P5H/exec",
+  apiBaseUrl: "https://script.google.com/macros/s/AKfycbwg1dhk4L51M2B9I7Cq3twVBi6SV9nQ9H9LEj6zCPlwa6APmMeemf-K5XIHUYjpjTvr/exec",
 };
